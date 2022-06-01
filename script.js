0f04c56ccc5ddcac69c9db3d6e5194580b75fd1b
RemcoDrolenga <rdrolenga@gmail.com>
2022-06-01 11:44:10 +0200

    opdracht afgerond

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var btn = document.createElement('button');
-btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.style.margin = '50px';
+btn.innerHTML = 'Switch light on';
 document.body.appendChild(btn);
-
+btn.onclick = Lightswitch
+document.body.style.backgroundColor = "black"
 // schijf hier tussen je code
 
+function Lightswitch(){
+if(btn.innerHTML == "Switch light off"){
+btn.innerHTML = "Switch light on"
+document.body.style.backgroundColor = "black"
+console.log("Light is off")
+}else{
+btn.innerHTML = "Switch light off"
+document.body.style.backgroundColor = "yellow"
+console.log("light is on")
+}
+
+}
 
 
-// schijf hier tussen je code
+// schijf hier tussen je code 
